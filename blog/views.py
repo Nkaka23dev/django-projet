2dc3e5920ff50ee75831d0b36a5fb5ef1faff243
Nkaka23dev <62412678+Nkaka23dev@users.noreply.github.com>
2020-12-27 22:25:52 -0800

    template adding

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts=[
+    {
+    'title':'My first blog',
+    'author':'Nkaka Eric',
+    'posted_date':'December,26,2020',
+    'content':'Hello everyone how are you!'
+    },
+    {
+    'title':'Greetings',
+    'author':'Jane Doe',
+    'posted_date':'november,2,2020',
+    'content':'I am jane doe what about you!'
+    },
+]
+
 def home(request):
-    return HttpResponse("<h1>The home of my blog is here.</h1>")
+    context={
+        'posts':posts
+    }
+    return render(request,'blog/home.html',context)
 
 def about(request):
-    return HttpResponse("<h1>About Page</h1>")
+    return render(request,'blog/about.html',{'title':'About'})
